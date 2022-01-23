507e08da15ca0eb3584fc950f9b1f35d9b7be58d
Marcin Weglowski <marcin.weglowski1@gmail.com>
2022-01-23 15:42:27 +0100

    start doing console-pymed-system

---
+++ main.py
@@ -0,0 +1,18 @@
+# System of e-commerce company that sells meds
+
+def showLandingPage():
+	print("|================|-------|================|\n|================| Pymed |================|\n|==============|           |==============|")
+	print("Welcome!")
+	print("Select option.")
+
+	print("1. Add order")
+	print("2. Edit order")
+	print("3. Show orders list")
+	print("4. Add client")
+	print("5. Edit client")
+	print("6. Show clients list")
+	print("0. Exit")
+
+	option = int(input("Enter number to select option >> "))
+
+showLandingPage()
